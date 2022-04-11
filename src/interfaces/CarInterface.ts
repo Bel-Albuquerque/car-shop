8bfe8a66f95e9b0b5f0d049d54e87400a59b5c5e
bel <mariaisabelalbuquerque@gmail.com>
2022-04-11 17:47:06 -0300

    cria CarInterface e implementa interface Car

---
+++ src/interfaces/CarInterface.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { VehicleInterface } from './VehicleInterface';
+
+export const CarInterface = VehicleInterface.extend({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+});
+
+export type Car = z.infer<typeof CarInterface>;
