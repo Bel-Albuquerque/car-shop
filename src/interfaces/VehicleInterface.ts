6221619762b57adc1db4c69ed2b852da739a1529
bel <mariaisabelalbuquerque@gmail.com>
2022-04-11 17:45:51 -0300

    coloca o export na const VehicleInterface

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const VehicleInterface = z.object({
+export const VehicleInterface = z.object({
   model: z.string().min(3),
   year: z.number().min(1900).max(2022),
   color: z.string().min(3),
