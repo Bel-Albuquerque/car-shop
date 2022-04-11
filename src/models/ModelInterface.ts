3cd00f3103d603ebd2a974cb922aa3958b1ae2f8
bel <mariaisabelalbuquerque@gmail.com>
2022-04-11 17:17:03 -0300

    cria model genérica

---
+++ src/models/ModelInterface.ts
@@ -0,0 +1,29 @@
+import { Model as M, Document } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class MongoModel<T> implements Model<T> {
+  constructor(protected model: M<T & Document>) { }
+
+  create = async (obj: T): Promise<T> => (
+    this.model.create({ ...obj }));
+
+  read = async (): Promise<T[]> => (
+    this.model.find());
+
+  readOne = async (id: string): Promise<T | null> => (
+    this.model.findOne({ _id: id }));
+
+  update = async (id: string, obj: T): Promise<T | null> => {
+    const result = this.readOne(id);
+    this.model.updateOne({ id, obj });
+    return result;
+  };
+
+  delete = async (id: string): Promise<T | null> => {
+    const result = this.readOne(id);
+    this.model.findOneAndDelete({ _id: id });
+    return result;
+  };
+}
+
+export default MongoModel;
