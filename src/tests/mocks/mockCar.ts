0ab1f905f71abc407d7c8f6a12b96437c3694d30
bel <mariaisabelalbuquerque@gmail.com>
2022-04-13 16:43:54 -0300

    pasta de mocks para testes

---
+++ src/tests/mocks/mockCar.ts
@@ -0,0 +1,19 @@
+export const createCar = {
+  "model": "Feretertrrari Maranello",
+  "year": 1993,
+  "color": "green",
+  "buyValue": 35000,
+  "seatsQty": 2,
+  "doorsQty": 2
+}
+
+export const createdCar = {
+  "model": "Feretertrrari Maranello",
+  "year": 1993,
+  "color": "green",
+  "buyValue": 35000,
+  "doorsQty": 2,
+  "seatsQty": 2,
+  "_id": "6254f09f1549daf7cec1035f",
+  "__v": 0
+}
