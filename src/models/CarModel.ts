0b51eceef29509952480c20f384f6da779f6bc68
bel <mariaisabelalbuquerque@gmail.com>
2022-04-13 16:45:07 -0300

    corrige caminho de importação de arquivo que foi renomeado

--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -1,8 +1,6 @@
-// src/Models/RecordStore.ts
-
 import { Schema, model as createModel, Document } from 'mongoose';
 import { Car } from '../interfaces/CarInterface';
-import MongoModel from './ModelInterface';
+import MongoModel from './MongoModel';
 
 interface CarDocument extends Car, Document { }
 
