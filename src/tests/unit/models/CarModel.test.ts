5651e3bd9fc7afa48694b11c74661c06b9effdb4
bel <mariaisabelalbuquerque@gmail.com>
2022-04-14 15:28:15 -0300

    adiciona testes - estão falhando

--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -1,23 +1,39 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import CarModel from '../../../models/CarModel';
-import { createCar, createdCar } from '../../mocks/mockCar'
+import { createCar, createdCar, allCars } from '../../mocks/mockCar'
+import { Car } from '../../../interfaces/CarInterface';
+import { Document } from 'mongoose';
+
+type returnType = Car & Document<any, any, any> & { _id: any; }
 
 describe('Rota /cars', () => {
   let carModel = new CarModel();
 
   before(() => {
-    sinon
-      .stub(carModel.model, 'create')
-      .resolves(createdCar);
+    sinon.stub(carModel.model, 'create').resolves(createdCar as returnType);
+    sinon.stub(carModel.model, 'find').resolves(allCars as returnType[]);
+      sinon
+      .stub(carModel.model, 'findOne')
+      .resolves(allCars as never);
+      sinon
+      .stub(carModel.model, 'updateOne')
+      .resolves(allCars as never);
+      sinon
+      .stub(carModel.model, 'findOneAndDelete')
+      .resolves(allCars as never);
   });
 
   after(() => {
     (carModel.model.create as sinon.SinonStub).restore();
+    (carModel.model.find as sinon.SinonStub).restore();
+    (carModel.model.findOne as sinon.SinonStub).restore();
+    (carModel.model.updateOne as sinon.SinonStub).restore();
+    (carModel.model.findOneAndDelete as sinon.SinonStub).restore();
   })
 
-  it('Testa se é possível fazer o login com dados corretos e que após o acesso será redirecionado para a tela de jogos', async () => {
-    const mockCar = await carModel.create(createCar)
+  it('teste se o método create do CarModel está implementado da maneira correta', async () => {
+    const mockCar = await carModel.create(createCar);
 
     expect(mockCar).to.be.an('object');
     expect(mockCar.model).to.be.equal(createdCar.model);
@@ -28,5 +44,10 @@ describe('Rota /cars', () => {
     expect(mockCar.doorsQty).to.be.equal(createdCar.doorsQty);
   });
 
-});
+  it('testa se o método read do CarModel está implementado da maneira correta', async () => {
+    const mockCar = await carModel.read();
 
+    expect(mockCar).to.have.lengthOf(2);
+  })
+
+});
