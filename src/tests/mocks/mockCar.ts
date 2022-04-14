82686fecbfe8db715d04f0044ee1c69ad09b53b4
bel <mariaisabelalbuquerque@gmail.com>
2022-04-14 16:54:07 -0300

    adicina mais mocks no arquivo de mockCar

--- src/tests/mocks/mockCar.ts
+++ src/tests/mocks/mockCar.ts
@@ -39,12 +39,22 @@ export const allCars = [
   }
 ]
 
-export const validCar = {
-  _id: new Types.ObjectId(),
-  model: 'Uno da Escada',
+export const findOneCar = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Fiat Uno",
   year: 1963,
-  color: 'red',
+  color: "blue",
   buyValue: 3500,
-  seatsQty: 2,
-  doorsQty: 2
+  seatsQty: 4,
+  doorsQty: 4
+};
+
+export const editedObj = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "honda fit",
+  year: 2000,
+  color: "blue",
+  buyValue: 9500,
+  seatsQty: 4,
+  doorsQty: 4
 };
