c36e96d808a679b227e7026f657443297b484220
bel <mariaisabelalbuquerque@gmail.com>
2022-04-15 16:28:38 -0300

    refatora e corrige alguns testes

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -5,10 +5,14 @@ import { createCar, createdCar, wrongObjectToCreation, allCars, findOneCar, edit
 import CarController from '../../../controllers/CarController';
 import { Car } from '../../../interfaces/CarInterface';
 import { RequestWithBody } from '../../../controllers';
+import StatusCode from '../../..//interfaces/StatusCode';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
+const {
+OK, CREATED, BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } = StatusCode;
+
 
 
 describe('testa camada Controller CarController', () => {
@@ -38,7 +42,7 @@ describe('testa camada Controller CarController', () => {
   it('teste se o método create do CarController está implementado da maneira correta', async () => {
    await carController.create(request, response);
 
-    expect((response.status).calledWith(201)).to.be.equal(true);
+    expect((response.status).calledWith(CREATED)).to.be.equal(true);
     expect((response.json).calledWith(createdCar)).to.be.equal(true);
   });
 
@@ -63,7 +67,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método create do CarController retorna erro quando recebe um objeto que não atende às regras de negócio', async () => {
    await carController.create(request, response);
 
-    expect(response.status.calledWith(400)).to.be.equal(true);
+    expect(response.status.calledWith(BAD_REQUEST)).to.be.equal(true);
     expect((response.json).getCall(0).args[0]).to.have.property('error');
   });
 
@@ -90,7 +94,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método read do CarController está implementado da maneira correta', async () => {
    await carController.read(request, response);
 
-    expect(response.status.calledWith(200)).to.be.equal(true);
+    expect(response.status.calledWith(OK)).to.be.equal(true);
     expect((response.json).calledWith(allCars)).to.be.equal(true);
   });
 
@@ -118,7 +122,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método readOne do CarController está implementado da maneira correta', async () => {
    await carController.readOne(request, response);
 
-    expect(response.status.calledWith(200)).to.be.equal(true);
+    expect(response.status.calledWith(OK)).to.be.equal(true);
     expect((response.json).calledWith(findOneCar)).to.be.equal(true);
   });
 
@@ -142,7 +146,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método readOne do CarController retorna erro quando não recebe o id valido no params', async () => {
    await carController.readOne(request, response);
 
-    expect(response.status.calledWith(500)).to.be.equal(true);
+    expect(response.status.calledWith(BAD_REQUEST)).to.be.equal(true);
     expect((response.json).getCall(0).args[0]).to.have.property('error');
   });
 });
@@ -170,7 +174,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método update do CarController está implementado da maneira correta', async () => {
    await carController.update(request, response);
 
-   expect((response.status).calledWith(201)).to.be.equal(true);
+   expect((response.status).calledWith(CREATED)).to.be.equal(true);
    expect((response.json).calledWith(editedObj)).to.be.equal(true);
   });
 });
@@ -195,7 +199,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método update do CarController retorna erro, caso o body seja invalido', async () => {
    await carController.update(request, response);
 
-   expect((response.status).calledWith(500)).to.be.equal(true);
+   expect((response.status).calledWith(INTERNAL_SERVER_ERROR)).to.be.equal(true);
    expect((response.json).getCall(0).args[0]).to.have.property('error');
   });
 });
@@ -224,7 +228,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método update do CarController retorna erro, caso o body seja invalido', async () => {
    await carController.update(request, response);
 
-   expect((response.status).calledWith(404)).to.be.equal(true);
+   expect((response.status).calledWith(NOT_FOUND)).to.be.equal(true);
    expect((response.json).getCall(0).args[0]).to.have.property('error');
   });
 });
@@ -251,7 +255,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método delete do CarController está implementado da maneira correta', async () => {
    await carController.delete(request, response);
 
-    expect(response.status.calledWith(201)).to.be.equal(true);
+    expect(response.status.calledWith(CREATED)).to.be.equal(true);
     expect((response.json).calledWith(findOneCar)).to.be.equal(true);
   });
 });
@@ -274,7 +278,7 @@ describe('testa camada Controller CarController', () => {
   it('testa se o método delete do CarController retorna erro caso o id seja passado de maneira incorreta', async () => {
    await carController.delete(request, response);
 
-   expect((response.status).calledWith(500)).to.be.equal(true);
+   expect((response.status).calledWith(INTERNAL_SERVER_ERROR)).to.be.equal(true);
    expect((response.json).getCall(0).args[0]).to.have.property('error');
   });
 
