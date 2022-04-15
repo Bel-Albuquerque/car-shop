135b78469f933f110981a71e936ac2eb06540846
bel <mariaisabelalbuquerque@gmail.com>
2022-04-15 16:11:36 -0300

    cria enum statusCode

---
+++ src/interfaces/StatusCode.ts
@@ -0,0 +1,11 @@
+enum Status {
+  OK = 200,
+  CREATED,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export default Status;
