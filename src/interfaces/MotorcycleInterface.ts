3c11fc62b9745f512f078677177161e06c5058d6
bel <mariaisabelalbuquerque@gmail.com>
2022-04-16 19:18:39 -0300

    adiciona valor mínimo para engineCapacity na MotorcycleInterface

--- src/interfaces/MotorcycleInterface.ts
+++ src/interfaces/MotorcycleInterface.ts
@@ -3,7 +3,7 @@ import { VehicleInterface } from './VehicleInterface';
 
 export const MotorcycleInterface = VehicleInterface.extend({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().max(2500).int(),
+  engineCapacity: z.number().min(1).max(2500).int(),
 });
 
 export type Motorcycle = z.infer<typeof MotorcycleInterface>;
