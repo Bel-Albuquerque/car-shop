005dd4ee6e393eb4e98f94dc2aa7fcee2b5c11a9
bel <mariaisabelalbuquerque@gmail.com>
2022-04-16 17:16:29 -0300

    corrige tipagem de category em model MotorcycleModel

--- src/models/MotorcycleModel.ts
+++ src/models/MotorcycleModel.ts
@@ -4,13 +4,21 @@ import MongoModel from './MongoModel';
 
 interface MotorcycleDocument extends Motorcycle, Document { }
 
+enum TypeCategory {
+  Street = 'Street',
+  Custom = 'Custom',
+  Trail = 'Trail',
+}
+// ref: https://github.com/Automattic/mongoose/issues/9535
+// ref: solução para a tipagem de category
+
 const motorcycleSchema = new Schema<MotorcycleDocument>({
   model: String,
   year: Number,
   color: String,
   status: Boolean,
   buyValue: Number,
-  category: String,
+  category: { type: String, enum: Object.values(TypeCategory) },
   engineCapacity: Number,
 }, { 
   versionKey: false });
